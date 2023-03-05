b3999cfa8c1c7c2282fa43a9cd1b04c86d571acc
Jhen <jhencorreia@gmail.com>
2023-03-05 20:16:31 -0300

    qualquer coisa

---
+++ main.js
@@ -0,0 +1,10 @@
+function openMenu() {
+    var open = document.getElementById('menu');
+
+    if (open.style.display === 'none') {
+        open.style.display = 'flex';
+    } else {
+        open.style.display = 'none';
+    }
+}
+
